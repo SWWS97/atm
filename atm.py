de33acb7914ca0534ea8358a8f08c7e785984074
SWWS97 <parkmon2@naver.com>
2025-01-09 17:37:59 +0900

    ✨ 영수증 보기 기능 추가
    사용자가 영수증 내역을 확인할 수 있도록 기능을 추가, 영수증 내역이 없을 경우 메시지를 출력

--- atm.py
+++ atm.py
@@ -38,6 +38,14 @@ while True:
         receipts.append(('출금', withdraw_amount, balance))
         print()
         print(f'출금하신 금액은 {withdraw_amount}원이고, 현재 잔액은 {balance}원 입니다')
-    
+    if num == "3":
+        #영수증에 값이 있을때, 값이 없을때
+        if receipts:  # 프로그램이 시작되자마자 3번을 눌렀을 경우 이 조건은 참인가요 거짓인가요?
+            print('===영수증===')
+            for i in receipts:#[("입금", deposit_amount, balance),('출금', withdraw_amount, balance)]
+                print(f'{i[0]}: {i[1]}원 | 잔액: {i[2]}원')
+        else:
+            print('영수증 내역이 없습니다.')                           
+
 print()
 print(f'서비스를 종료합니다. 현재 잔액은 {balance}원입니다.')
